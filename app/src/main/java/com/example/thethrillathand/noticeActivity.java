efae38bfea048ac822a9d3d9b18184d49720f274
KyungChang <rudckd135@naver.com>
2020-06-11 16:44:17 +0900

    V0.1 SOS 기능 구현 완성

---
+++ app/src/main/java/com/example/thethrillathand/noticeActivity.java
@@ -0,0 +1,20 @@
+package com.example.thethrillathand;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class noticeActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_notice);
+
+    }
+
+
+}
